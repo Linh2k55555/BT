fb6433467e5253c300fe5aa71c788d335b814284
Linh2k55555 <107355752+Linh2k55555@users.noreply.github.com>
2025-01-18 17:15:10 +0700

    re

--- BT/checkPassword.js
+++ BT/checkPassword.js
@@ -1,8 +1,8 @@
 import bcryptjs from "bcryptjs";
 
 const testPassword = async () => {
-    const password = "123456"; // Mật khẩu bạn đang nhập
-    const hash = "$2a$10$84BZgUuoRYZXSrSJCMjI0eU.jyvryl8VydhUa98pFdU6id9CQdTKy"; // Hash từ DB
+    const password = "111111"; // Mật khẩu bạn đang nhập
+    const hash = "$2a$10$uCxytt5OZ2w0XVSL0m9Jou5LICbekUorGxbl8j9xwVz9iqsHSTKPy"; // Hash từ DB
 
     const isValid = await bcryptjs.compare(password, hash);
     console.log("Mật khẩu hợp lệ:", isValid); // Kết quả phải là true nếu mật khẩu khớp
--- BT/hashPassword.js
+++ BT/hashPassword.js
@@ -1,7 +1,7 @@
 import bcryptjs from "bcryptjs";
 
 const hashPassword = async () => {
-    const password = "123456"; // Mật khẩu bạn muốn hash
+    const password = "111111"; // Mật khẩu bạn muốn hash
     const hashedPassword = await bcryptjs.hash(password, 10); // Tạo hash mới
     console.log("Hashed Password:", hashedPassword);
 };
